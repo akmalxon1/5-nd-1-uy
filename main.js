e369ef75f088b7a5199a9578311c78c4a049c4e4
akmalxon1 <akmalxon359@gmail.com>
2024-08-10 23:57:37 +0500

    hello world

---
+++ main.js
@@ -0,0 +1,29 @@
+let a = +prompt(`Birdan 7 gacha son kiriting😁`)
+switch (a) {
+    case 1:
+        alert('Dushanba')
+        break;
+    case 2:
+        alert('Seyshanba')
+        break;
+    case 3:
+        alert('Chorshanba')
+        break;
+    case 4:
+        alert('Payshanba')
+        break;
+    case 5:
+        alert('Juma')
+        break;
+        case 6:
+            alert('Shanba')
+            break;
+        case 7:
+            alert('Yakshanba')
+            break;
+            case 8:
+                alert('Bir haftada 7 kun bor :-)')
+                break;
+    default:
+        break;
+}
